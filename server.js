b177ded9432af4b9f9ffd8d4520de5bce858cadc
Le Huong <Dongnguyen210300@gmail.com>
2025-12-19 22:21:33 +0700

    env

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config({quiet : true});
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
@@ -7,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 // Kết nối MongoDB với username là MSSV, password là MSSV, dbname là it4409 
 mongoose
-    .connect("mongodb+srv://20225199:20225199@cluster0.smnagnh.mongodb.net/?appName=Cluster0")
+    .connect(process.env.MONGO_URI)
     .then(() => console.log("Connected to MongoDB"))
     .catch((err) => console.error("MongoDB Error:", err));
 // TODO: Tạo Schema 
@@ -131,6 +132,7 @@ app.delete("/api/users/:id", async (req, res) => {
     }
 });
 // Start server 
-app.listen(3001, () => {
+PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
     console.log("Server running on http://localhost:3001");
 });
